bc8fa2a33ebe12eede3d48bf9fe53baa35936fe6
문승재 <gaeddongie13@gmail.com>
2021-01-25 23:20:35 +0900

    update BOJ Problems

---
+++ BOJ/10998_A곱B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,mul;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        mul = a * b;
+        System.out.println(mul);
+    }
+}
